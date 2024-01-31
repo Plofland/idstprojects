de624f5f7b6c0db36615256a7b367dbed2137db5
Plofland <ploflan@gmail.com>
2024-01-30 23:56:37 -0500

    add hero image, put navbar on top

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Link from 'next/link';
 import styled from 'styled-components';
 import Navbar from '@/components/navbar';
 import ResetStyles from '@/styles/globals';
@@ -21,11 +19,54 @@ export default function Home() {
 				/>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-      <ResetStyles />
+			<ResetStyles />
 			<main>
-				<Navbar />
+				<NavContainer>
+					<Navbar />
+				</NavContainer>
+				<HeroSection>
+					<HeroImage
+						src="/luxury-builder-new-york-city-04.jpg"
+						alt="Hero Image Alt Text"
+						layout="fill"
+						objectFit="cover"
+					/>
+					<HeroContent>
+						<h1>Your Hero Content Goes Here</h1>
+						<p>
+							Additional information or
+							call-to-action
+						</p>
+					</HeroContent>
+				</HeroSection>
 			</main>
 		</>
 	);
 }
 
+const NavContainer = styled.div`
+	background-color: #333;
+	color: white;
+	padding: 10px;
+	position: fixed;
+	width: 100%;
+	z-index: 100; /* Ensure Navbar is on top of other elements */
+`;
+
+const HeroSection = styled.section`
+	position: relative;
+	height: 100vh;
+`;
+
+const HeroImage = styled(Image)`
+	/* Optional: Apply additional styles to the image if needed */
+`;
+
+const HeroContent = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	text-align: center;
+	color: #fff; /* Set text color */
+`;
